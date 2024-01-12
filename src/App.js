cb69dc9136a73f4ab2e8f56c877392888bc9c55f
dannybartolini <dannybart22@gmail.com>
2024-01-12 09:06:47 -0500

    more navbar progress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import ListingsList from "./components/ListingTest";
+import BuyersList from "./components/BuyersTest";
+import Home from "./components/Home";
 
 function App() {
+	console.log(window.location.pathname);
+	let Component;
+	switch (window.location.pathname) {
+		case "/":
+			Component = Home;
+			break;
+		case "/listings":
+			Component = ListingsList;
+			break;
+		case "/buyers":
+			Component = BuyersList;
+			break;
+	}
 	return (
 		<div className="App">
 			<Navbar />
+
 			<h1>Hello there, this is a real estate app</h1>
-			<ListingsList />
+			<Component />
 		</div>
 	);
 }
