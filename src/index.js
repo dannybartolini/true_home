97b0832441f195c730bfe27da130b7fa2b6ad574
dannybartolini <dannybart22@gmail.com>
2024-04-23 09:44:45 -0400

    idk-been a while

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore, Tuple } from "@reduxjs/toolkit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
